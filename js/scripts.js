e9e1066ed77bb408d5d525d7caf506f9dbcf54b1
yus-torres <info@synthesisgroup.co>
2021-10-18 21:01:06 +0800

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+//Loader
+$(window).load(function() {
+	// Animate loader off screen
+	$(".se-pre-con").fadeOut("slow");;
+});
+
+//Add/remove class on page scroll
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 500) {
+        $("header").addClass("append");
+    } else {
+        $("header").removeClass("append");
+    }
+});
+
+//Add/remove class on page scroll
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1000) {
+        $(".utilities").addClass("show");
+    } else {
+        $(".utilities").removeClass("show");
+    }
+});
+
+// Medical Department Carousel
+$(document).on('ready', function() {
+	$(".medical-departments-carousel").slick({
+		infinite: true,
+		prevArrow: '<button type="button" class="slick-prev"><i class="fal fa-angle-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fal fa-angle-right"></i></button>',
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		responsive: [
+			{
+			breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+			breakpoint: 560,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+			breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 2
+				}
+			},
+			{
+			breakpoint: 320,
+					settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+});
